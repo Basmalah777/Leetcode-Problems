f3a348fafbdc4b44e55519ef4ecf935060ae0ea2
Basmalah777 <basmalahhithnawe@gmail.com>
2025-01-16 23:57:15 +0200

    Added Two Sum solution

--- TwoSum.cs
+++ TwoSum.cs
@@ -1,1 +1,32 @@
+using System;
+using System.Collections.Generic;
 
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            if (map.ContainsKey(complement))
+            {
+                return new int[] { map[complement], i };
+            }
+            map[nums[i]] = i;
+        }
+        throw new ArgumentException("No two sum solution");
+    }
+}
+//test
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = solution.TwoSum(nums, target);
+        Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
+    }
+}
